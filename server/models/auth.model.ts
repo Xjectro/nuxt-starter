8a58654b623892eb9714ff4c74678f39edc9c447
Xjectro <129968185+Xjectro@users.noreply.github.com>
2024-09-30 01:58:07 +0300

    Update auth.model.ts

--- server/models/auth.model.ts
+++ server/models/auth.model.ts
@@ -11,7 +11,7 @@ export enum Role {
 
 export interface AuthType extends mongoose.Document {
   _id: mongoose.Schema.Types.ObjectId;
-  user: any;
+  user: { _id: mongoose.Schema.Types.ObjectId };
   refresh_token: string;
   email: string;
   password: string;
